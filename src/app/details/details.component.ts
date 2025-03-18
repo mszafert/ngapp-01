cc2eb359a367db617b3d3f1bf002a8295af65eff
Marcin Szafert <marcin.szafert@xebia.com>
2025-03-18 20:48:56 +0100

    ADD app form & UPDATE styles

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,10 +2,11 @@ import { HousingService } from './../housing.service';
 import { ActivatedRoute } from '@angular/router';
 import { HousingLocation } from './../housing-location';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -13,9 +14,22 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
