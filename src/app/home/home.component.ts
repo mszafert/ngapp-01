934715f85e2000bf04c40da094b1dc3cb85ecab5
Marcin Szafert <marcin.szafert@xebia.com>
2025-03-19 15:32:51 +0100

    ADD attributify style preset & filter feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,11 +12,21 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
   housingService = inject(HousingService);
 
   constructor() {
     this.housingService.getAllHousinglocations().then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
     });
+  }
+
+  filterResults(text: string) {
+    if (!text) this.filteredLocationList = this.housingLocationList
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
